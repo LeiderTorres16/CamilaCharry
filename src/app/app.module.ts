99e230c05c15d5cf66f3b4d3d29980aba055d254
Rishard <rikelpa2001@gmail.com>
2023-07-28 19:47:27 -0500

    Vinculacion Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,16 +20,8 @@ import { FooterComponent } from './Modulos/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase )),
-    provideFirestore(() => getFirestore()),
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireStorageModule
-  ],
-  providers: [
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
