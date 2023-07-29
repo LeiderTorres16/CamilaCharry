51cd1ae3630da9a4ebd3f40df6be4c47a2836578
leider torres <leiderandres2002@hotmail.com>
2023-07-28 19:05:12 -0500

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './Modulos/principal/principal.component';
+import { HeaderComponent } from './Modulos/header/header.component';
+import { FooterComponent } from './Modulos/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
