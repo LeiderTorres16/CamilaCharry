a4b2d22d481c09605513d8decf4b11dd0cc6d975
Rishard <rikelpa2001@gmail.com>
2023-08-04 10:54:11 -0500

    app module correcto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './Modulos/principal/principal.component';
 import { HeaderComponent } from './Modulos/header/header.component';
 import { FooterComponent } from './Modulos/footer/footer.component';
+import { HomeComponent } from './Modulos/Contenidos/home/home.component';
+import { SuperiorComponent } from './Modulos/superior/superior.component';
+import { CarritoComponent } from './Modulos/Contenidos/carrito/carrito.component';
+import { FormsModule } from '@angular/forms';
+import { RegistroPrendaComponent } from './Modulos/Contenidos/registro-prenda/registro-prenda.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from "@angular/fire/compat/";
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 
 @NgModule({
   declarations: [
@@ -21,7 +33,14 @@ import { FooterComponent } from './Modulos/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase )),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule
+  ],
+  providers: [
   ],
   bootstrap: [AppComponent]
 })
