0cec6fa09f22d49aeb3d1c2ececae961a40b31f1
Rishard <rikelpa2001@gmail.com>
2023-08-04 10:54:11 -0500

    app module correcto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './Modulos/principal/principal.component';
 import { HeaderComponent } from './Modulos/header/header.component';
 import { FooterComponent } from './Modulos/footer/footer.component';
-import { HomeComponent } from './Modulos/Contenidos/home/home.component';
-import { SuperiorComponent } from './Modulos/superior/superior.component';
-import { CarritoComponent } from './Modulos/Contenidos/carrito/carrito.component';
-import { FormsModule } from '@angular/forms';
-import { RegistroPrendaComponent } from './Modulos/Contenidos/registro-prenda/registro-prenda.component';
-
 
 @NgModule({
   declarations: [
@@ -27,13 +22,7 @@ import { RegistroPrendaComponent } from './Modulos/Contenidos/registro-prenda/re
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
-  ],
-  providers: [
-
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
