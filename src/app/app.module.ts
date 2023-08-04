4d84c29c71254c815eff7217bfadd3e7a964da30
leider torres <leiderandres2002@hotmail.com>
2023-08-04 10:35:07 -0500

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './Modulos/principal/principal.component';
 import { HeaderComponent } from './Modulos/header/header.component';
 import { FooterComponent } from './Modulos/footer/footer.component';
-import { HomeComponent } from './Modulos/Contenidos/home/home.component';
-import { SuperiorComponent } from './Modulos/superior/superior.component';
-import { CarritoComponent } from './Modulos/Contenidos/carrito/carrito.component';
-import { FormsModule } from '@angular/forms';
-import { RegistroPrendaComponent } from './Modulos/Contenidos/registro-prenda/registro-prenda.component';
-
 
 @NgModule({
   declarations: [
