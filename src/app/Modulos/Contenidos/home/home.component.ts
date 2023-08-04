898316560b5707a996cb1c8b9daa3fe2b7366fd0
leider torres <leiderandres2002@hotmail.com>
2023-08-04 10:29:17 -0500

    Lista de Producto

--- src/app/Modulos/Contenidos/home/home.component.ts
+++ src/app/Modulos/Contenidos/home/home.component.ts
@@ -3,8 +3,103 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  productosDestacados: any[] = [];
+  productosEspeciales: any[] = [];
 
+  constructor() {
+    // Agregar algunos productos de ejemplo al carrito
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Blanca',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Negra',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Blanca',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Blanca',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Blanca',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+    this.agregarProducto({
+      imagen: '../../../assets/images/c_formal_gray_shirt.png',
+      nombreProducto: 'Camiseta Blanca',
+      codigo: 'CM001',
+      precio: 99.99,
+    });
+
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
+    });
+  }
+
+  agregarProducto(producto: any) {
+    this.productosDestacados.push(producto);
+  }
+
+  agregarProductoE(producto: any) {
+    this.productosEspeciales.push(producto);
+  }
 }
