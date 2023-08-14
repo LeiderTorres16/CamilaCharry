40a6f660ed72b4f2f21c97b710a9b49defca8d15
leider torres <leiderandres2002@hotmail.com>
2023-08-14 11:44:08 -0500

    AJustes

--- src/app/Modulos/Contenidos/home/home.component.ts
+++ src/app/Modulos/Contenidos/home/home.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { PrendasService } from 'src/app/Services/prendas_Service';
 import { Prenda } from 'src/app/Models/prenda_class';
-=======
 import { Router } from '@angular/router';
->>>>>>> be19ef8 (Detalle prenda)
 
 @Component({
   selector: 'app-home',
@@ -15,36 +12,7 @@ export class HomeComponent {
   productosDestacados: any[] = [];
   productosEspeciales: any[] = [];
 
-  constructor(private prendasService: PrendasService) {
-    // Agregar algunos productos de ejemplo al carrito
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Blanca',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Negra',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Blanca',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Blanca',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
+  constructor(private prendasService: PrendasService, private router: Router) {
 
     this.agregarProductoE({
       imagen: '../../../assets/images/c_western-shirt.png',
@@ -52,27 +20,11 @@ export class HomeComponent {
       precio: 45.50,
     });
 
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Negra',
-      codigo: 'CM001',
-      precio: 99.99,
+    this.agregarProductoE({
+      imagen: '../../../assets/images/c_western-shirt.png',
+      nombreProducto: 'Camisa Gris',
+      precio: 45.50,
     });
-
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Blanca',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
-    this.agregarProducto({
-      imagen: '../../../assets/images/c_formal_gray_shirt.png',
-      nombreProducto: 'Camiseta Blanca',
-      codigo: 'CM001',
-      precio: 99.99,
-    });
-
 
     this.agregarProductoE({
       imagen: '../../../assets/images/c_western-shirt.png',
@@ -87,9 +39,25 @@ export class HomeComponent {
 
   agregarProductoE(producto: any) {
     this.productosEspeciales.push(producto);
+  }
 
+  detallePrenda(){
+    this.router.navigateByUrl('/DetallePrenda');
   }
   detallePrenda(){
     this.router.navigateByUrl('/DetallePrenda');
+  }
+  detallePrenda(){
+    this.router.navigateByUrl('/DetallePrenda');
+  }
+
+  ngOnInit():void{
+    this.prendasService.getPrendas().subscribe(prendas=> {
+      
+      prendas.forEach(prenda => {
+        this.agregarProducto(prenda);
+      });    
+        console.log(prendas);
+    })
   }
 }
